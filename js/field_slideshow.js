edd8a8ab633ef62f37ccec649d5fbf55caa0d2b8
Jérôme Danthinne <jerome@grincheux.be>
2011-03-24 08:59:32 +0100

    fixed : added 'processed' class to the slideshow so the scripts are not applied more than once. Re-moved all markup creation in the .module file, instead of .js.

--- js/field_slideshow.js
+++ js/field_slideshow.js
@@ -6,49 +6,48 @@
         var settings = Drupal.settings.field_slideshow[i];
         var slideshow = $('.' + i);
 
-        // Add padding if needed
-        var max_outerWidth = 0;
-        var max_outerHeight = 0;
-        $('.field-slideshow-slide', slideshow).each(function() {
-          $this = $(this);
-          max_outerWidth = Math.max(max_outerWidth, $this.outerWidth(true));
-          max_outerHeight = Math.max(max_outerHeight, $this.outerHeight(true));
-        });
-        slideshow.css({
-          'padding-right': (max_outerWidth - parseInt(slideshow.css('width'))) + 'px',
-          'padding-bottom': (max_outerHeight - parseInt(slideshow.css('height'))) + 'px'
-        });
+        if (!slideshow.hasClass('field-slideshow-processed')) {
+          slideshow.addClass('field-slideshow-processed');
 
-        // Create Pager wrapper (if not image)
-        if (settings.pager != '' && settings.pager != 'image') slideshow.after("<div id='" + i + "-pager' class='field-slideshow-pager'/>");
+          // Add padding if needed
+          var max_outerWidth = 0;
+          var max_outerHeight = 0;
+          $('.field-slideshow-slide', slideshow).each(function() {
+            $this = $(this);
+            max_outerWidth = Math.max(max_outerWidth, $this.outerWidth(true));
+            max_outerHeight = Math.max(max_outerHeight, $this.outerHeight(true));
+          });
+          slideshow.css({
+            'padding-right': (max_outerWidth - parseInt(slideshow.css('width'))) + 'px',
+            'padding-bottom': (max_outerHeight - parseInt(slideshow.css('height'))) + 'px'
+          });
 
-        // Create Controls
-        if (settings.controls) slideshow.after("<div id='" + i + "-controls' class='field-slideshow-controls'><a href='#' class='prev'>" + Drupal.t('Prev') + "</a> <a href='#' class='next'>" + Drupal.t('Next') + "</a></div>");
+          // Add options
+          var options = {
+            resizing: 0,
+            fx: settings.fx,
+            speed: settings.speed,
+            timeout: parseInt(settings.timeout)
+          }
 
-        // Add options
-        var options = {
-          resizing: 0,
-          fx: settings.fx,
-          speed: settings.speed,
-          timeout: parseInt(settings.timeout)
+          if (settings.speed == "0" && settings.timeout == "0") options.fastOnEvent = true;
+          if (settings.controls != "0") {
+            options.prev = "#" + i + "-controls .prev";
+            options.next = "#" + i + "-controls .next";
+          }
+          if (settings.pause != "0") options.pause = true;
+
+          if (settings.pager != '') {
+            options.pager = "#" + i + "-pager";
+            if (settings.pager == 'image') options.pagerAnchorBuilder = function(idx, slide) {
+              return '#' + i + '-pager li:eq(' + idx + ') a';
+            };
+          }
+
+          // Cycle!
+          slideshow.cycle(options); 
+
         }
-
-        if (settings.speed == "0" && settings.timeout == "0") options.fastOnEvent = true;
-        if (settings.controls != "0") {
-          options.prev = "#" + i + "-controls .prev";
-          options.next = "#" + i + "-controls .next";
-        }
-        if (settings.pause != "0") options.pause = true;
-
-        if (settings.pager != '') {
-          options.pager = "#" + i + "-pager";
-          if (settings.pager == 'image') options.pagerAnchorBuilder = function(idx, slide) {
-            return '#' + i + '-pager li:eq(' + idx + ') a';
-          };
-        }
-
-        // Cycle!
-        slideshow.cycle(options);
 
       }
 
